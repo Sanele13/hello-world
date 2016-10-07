685b6a75568f97578dc066be70d4f13eff77ac86
Sanele13 <aynstyn@gmail.com>
2016-10-07 16:33:52 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class HelloWorld{
-  public static void main(){
+  public static void main( String [] args){
     System.out.println("Hello World!");
   }
-
 }
